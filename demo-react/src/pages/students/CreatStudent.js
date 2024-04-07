be5cf9226e3af4af75519f799c071aa76841716a
LocTM <tranminhloc.anhk@gmail.com>
2024-04-07 20:37:09 +0700

    test navigate

--- demo-react/src/pages/students/CreatStudent.js
+++ demo-react/src/pages/students/CreatStudent.js
@@ -1,7 +1,16 @@
+import {useNavigate} from "react-router-dom";
+
 export function CreatStudent(){
+    const navigate = useNavigate()
     return(
         <>
             <h1>Create Student</h1>
+            <button onClick={() => {
+                navigate('/')
+            }}>Quay lai trang chu</button>
+            <button onClick={()=>{
+                navigate('/admin')
+            }}>Vao trang Admin</button>
         </>
     )
 }
