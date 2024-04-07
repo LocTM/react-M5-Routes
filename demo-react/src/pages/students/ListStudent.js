6624636bf549ee6c79dfcf7daab743c2d42376b7
LocTM <tranminhloc.anhk@gmail.com>
2024-04-07 21:20:34 +0700

    test axios

--- demo-react/src/pages/students/ListStudent.js
+++ demo-react/src/pages/students/ListStudent.js
@@ -1,12 +1,22 @@
 import {useLocation} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export function ListStudent(){
-    const {state} = useLocation();
-    console.log(state)
+    const [list,setList] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:3001/students").then(res => {
+            setList(res.data)
+        })
+    }, []);
+
 
     return(
         <>
             <h1>List Student</h1>
+            {list.map((item,key) => (
+                <h3>{item.name} : {item.description} : {item.action} : {item.score}</h3>
+            ))}
         </>
     )
 }
